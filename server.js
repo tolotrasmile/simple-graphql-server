22fdd1b3a360af8297a743f10cebfee8cfe255ab
TolotraSmile <tolotra.raharison@gmail.com>
2018-03-07 15:26:48 +0300

    rename GraphQL

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const express = require('express')
 const schema = require('./schema')
-const expressGraphQL = require('express-graphql')
+const graphqlHTTP = require('express-graphql')
 
 const app = express()
 
-app.use('/api', expressGraphQL({
-  schema: schema,
-  graphiql: true
-}))
+app.use('/api', graphqlHTTP({ schema: schema, graphiql: true }))
 
 app.listen(4000, () => {
   console.log('Server is running on port 4000..')
